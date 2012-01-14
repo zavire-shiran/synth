93c899135de00a4cef00896ac3423ebf278b34f1
Zavire Shiran <zavire.shiran@gmail.com>
2012-01-13 23:08:19 -0600

    Added simple envelope, and signal multiply.

--- music.hs
+++ music.hs
@@ -3,10 +3,10 @@ import Data.Int
 import Data.Binary as Binary
 import qualified Data.ByteString.Lazy as BS
 
-main :: IO ()
-main = BS.putStr $ serializeSignal $ take 44100 $ genSineWave (SignalAttributes 44100) 440
+type Signal = [Float]
 
-data SignalFormat = UnsignedInteger | Float
+main :: IO ()
+main = BS.putStr $ serializeSignal $ multiplySignals (genSineWave (SignalAttributes 44100) 440) (genEnvelope (SignalAttributes 44100) 2 0.01 0.01)
 
 data SignalAttributes = SignalAttributes {
   sampleRate :: Float
@@ -15,6 +15,15 @@ data SignalAttributes = SignalAttributes {
 genSineWave :: SignalAttributes -> Float -> [Float]
 genSineWave sa@(SignalAttributes sr) freq = generateSineWave sr freq 0
   where generateSineWave sr freq theta = sin theta : (generateSineWave sr freq $ theta + (pi * freq / sr))
+
+genEnvelope :: SignalAttributes -> Float -> Float -> Float -> [Float]
+genEnvelope sa@(SignalAttributes sr) len indur outdur = 0 : takeWhile (>0) (map (generateEnvelope sr len indur outdur) [1..])
+  where generateEnvelope sr len indur outdur i = let curTime = i / sr in case () of _ | curTime < indur -> curTime / indur
+                                                                                      | curTime < len - outdur -> 1.0
+                                                                                      | otherwise  -> (len - curTime) / outdur
+
+multiplySignals :: Signal -> Signal -> Signal
+multiplySignals one two = zipWith (*) one two
 
 serializeSignal :: [Float] -> BS.ByteString
 serializeSignal signal = BS.concat $ map (Binary.encode . (truncate :: Float -> Int32) . (*1e9)) signal
