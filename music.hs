3bc0dcb862cd876d8916f8a82e46f6db7beda72b
Zavire Shiran <zavire.shiran@gmail.com>
2012-01-15 16:46:27 -0600

    Refactored genSineWave, and made it easier to make a bunch of sine waves together.

--- music.hs
+++ music.hs
@@ -5,16 +5,24 @@ import qualified Data.ByteString.Lazy as BS
 
 type Signal = [Float]
 
+nullSignal :: Signal
+nullSignal = repeat 0
+
+
 main :: IO ()
-main = BS.putStr $ serializeSignal $ multiplySignals (genSineWave (SignalAttributes 44100) 440) (genEnvelope (SignalAttributes 44100) 2 0.01 0.01)
+main = let signal = genCompositeSineWave (SignalAttributes 44100) [(440, 1.0), (880, 0.5), (1320, 0.25), (1760, 0.125)] in
+  BS.putStr $ serializeSignal $ multiplySignals signal (genEnvelope (SignalAttributes 44100) 2 0.01 0.01)
 
 data SignalAttributes = SignalAttributes {
   sampleRate :: Float
 }
 
-genSineWave :: SignalAttributes -> Float -> [Float]
-genSineWave sa@(SignalAttributes sr) freq = generateSineWave sr freq 0
-  where generateSineWave sr freq theta = sin theta : (generateSineWave sr freq $ theta + (pi * freq / sr))
+genCompositeSineWave :: SignalAttributes -> [(Float, Float)] -> Signal
+genCompositeSineWave sa wavedef = foldl addSignals nullSignal $ map makeSine_ wavedef
+  where makeSine_ (f, g) = gain g $ genSineWave sa f
+
+genSineWave :: SignalAttributes -> Float -> Signal
+genSineWave sa@(SignalAttributes sr) freq = map (sin . (* (pi * freq / sr))) [0..]
 
 genEnvelope :: SignalAttributes -> Float -> Float -> Float -> [Float]
 genEnvelope sa@(SignalAttributes sr) len indur outdur = 0 : takeWhile (>0) (map (generateEnvelope sr len indur outdur) [1..])
@@ -22,8 +30,14 @@ genEnvelope sa@(SignalAttributes sr) len indur outdur = 0 : takeWhile (>0) (map 
                                                                                       | curTime < len - outdur -> 1.0
                                                                                       | otherwise  -> (len - curTime) / outdur
 
+gain :: Float -> Signal -> Signal
+gain g s = map (*g) s
+
+addSignals :: Signal -> Signal -> Signal
+addSignals one two = zipWith (+) one two
+
 multiplySignals :: Signal -> Signal -> Signal
 multiplySignals one two = zipWith (*) one two
 
 serializeSignal :: [Float] -> BS.ByteString
-serializeSignal signal = BS.concat $ map (Binary.encode . (truncate :: Float -> Int32) . (*1e9)) signal
+serializeSignal signal = BS.concat $ map (Binary.encode . (truncate :: Float -> Int32) . (*1.5e8)) signal
