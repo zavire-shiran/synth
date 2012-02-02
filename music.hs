cbbf264989e217f9c2d2d6d0a3028a0a34d88aa3
Zavire Shiran <zavire.shiran@gmail.com>
2012-02-01 21:29:59 -0600

    Something vaguely resembling an instrument interface

--- music.hs
+++ music.hs
@@ -1,6 +1,6 @@
 module Main where
 import Data.Int
-import Data.Binary as Binary
+import qualified Data.Binary as Binary
 import qualified Data.ByteString.Lazy as BS
 
 type Signal = [Float]
@@ -12,8 +12,8 @@ defaultSigAttr :: SignalAttributes
 defaultSigAttr = SignalAttributes 44100
 
 main :: IO ()
-main = let signal1 = applyEnvelope defaultSigAttr 1 0.01 0.01 (genCompositeSineWave defaultSigAttr [(220, 1.0), (440, 0.5), (880,  0.25), (1320, 0.125)])
-           signal2 = applyEnvelope defaultSigAttr 1 0.01 0.01 (genCompositeSineWave defaultSigAttr [(440, 1.0), (880, 0.5), (1320, 0.25), (1760, 0.125)]) in
+main = let signal1 = allHarmonicsWave defaultSigAttr 1.0 220
+           signal2 = allHarmonicsWave defaultSigAttr 1.0 $ pitchToFrequency 69 in
   BS.putStr $ serializeSignal $ appendSignals signal1 signal2
 
 data SignalAttributes = SignalAttributes {
@@ -50,3 +50,24 @@ appendSignals = (++)
 
 serializeSignal :: [Float] -> BS.ByteString
 serializeSignal signal = BS.concat $ map (Binary.encode . (truncate :: Float -> Int32) . (*1.5e8)) signal
+
+makeTuples :: [a] -> [(a,a)]
+makeTuples (x:y:xs) = (x,y) : makeTuples xs
+makeTuples (x:[]) = []
+makeTuples [] = []
+
+sineInstr :: SignalAttributes -> Float -> [(Float, Float)] -> Signal
+sineInstr sa length attrs = applyEnvelope sa length 0.01 0.01 $ genCompositeSineWave sa attrs
+
+pitchToFrequency :: Float -> Float
+pitchToFrequency pitch = 440 * (2 ** ((pitch - 69) / 12))
+
+allHarmonicsWave :: SignalAttributes -> Float -> Float -> Signal
+allHarmonicsWave sa length freq = sineInstr sa length $ map (\h -> (h * freq, 1/h)) [1..20]
+
+approxSquareWave :: SignalAttributes -> Float -> Float -> Signal
+approxSquareWave sa length freq = sineInstr sa length $ map (\h -> (h * freq, 1/h)) [1, 3..19]
+
+approxSquareInstr :: SignalAttributes -> [Float] -> Signal
+approxSquareInstr sa [l, f] = approxSquareWave sa l f
+
