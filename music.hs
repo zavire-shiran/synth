25823595c7cbb5c5c8504babdfbfae306ce31844
Zavire Shiran <zavire.shiran@gmail.com>
2013-11-19 14:13:22 -0600

    Remove remnant compositon/instrument infrastructure. Add sine and triangle wave generators capable of FM.

--- music.hs
+++ music.hs
@@ -23,27 +23,41 @@ defaultSigAttr :: SignalAttributes
 defaultSigAttr = SignalAttributes 44100
 
 main :: IO ()
---main = let s1 = allHarmonicsWave defaultSigAttr 1.0 (pitchToFrequency 60)
---           s2 = allHarmonicsWave defaultSigAttr 1.0 (pitchToFrequency 64)
---           s3 = allHarmonicsWave defaultSigAttr 1.0 (pitchToFrequency 67) in
---          BS.putStr . serializeSignal $ sumSignals [s1, s2, s3]
+--main = let s1 = approxTriangleWave defaultSigAttr 1.0 (pitchToFrequency 60)
+--           s2 = approxTriangleWave defaultSigAttr 1.0 (pitchToFrequency 64)
+--           s3 = approxTriangleWave defaultSigAttr 1.0 (pitchToFrequency 67) in
+--         BS.putStr . serializeSignal $ gain 4 $ sumSignals [s1, s2, s3]
 
---main = BS.putStr . serializeSignal $ sineWave defaultSigAttr 1.0 (pitchToFrequency 80)
-
-main = readComposition "composition" >>= print 
+main = BS.putStr . serializeSignal $ applyEnvelope defaultSigAttr 1.0 0.01 0.01 $ gain 3 $ genTriangleWave defaultSigAttr (addSignals (repeat $ pitchToFrequency 48) (gain 100 $ genSineWave defaultSigAttr (repeat $ pitchToFrequency 90)))
 
 data SignalAttributes = SignalAttributes {
   sampleRate :: Float
 }
 
-type Composition = [Sequence]
-
 genCompositeSineWave :: SignalAttributes -> [(Float, Float)] -> Signal
 genCompositeSineWave sa wavedef = foldl addSignals nullSignal $ map makeSine_ wavedef
-  where makeSine_ (f, g) = gain g $ genSineWave sa f
+  where makeSine_ (f, g) = gain g $ genSineWave sa (repeat f)
 
-genSineWave :: SignalAttributes -> Float -> Signal
-genSineWave sa@(SignalAttributes sr) freq = map (sin . (* (pi * freq / sr))) [0..]
+wrap :: Float -> Float -> Float -> Float
+wrap min max val =
+  if val < min then
+    val + (max - min)
+  else if val > max then
+    val - (max - min)
+  else
+    val
+
+genSineWave :: SignalAttributes -> Signal -> Signal
+genSineWave sa@(SignalAttributes sr) freq = inner 0 freq
+            where inner phase (f:rest) = sin (phase * 2 * pi) : inner (wrap 0 1 (phase + f / sr)) rest
+
+genTriangleWave :: SignalAttributes -> Signal -> Signal
+genTriangleWave sa@(SignalAttributes sr) freq = inner 0 freq
+            where inner phase (f:rest) = waveform phase : inner (wrap 0 1 (phase + f / sr)) rest
+                  waveform p = case () of _
+                                           | p < 0.25 -> p * 4
+                                           | p < 0.75 -> 1 - ((p - 0.25) * 4)
+                                           | otherwise -> (p-1) * 4
 
 genEnvelope :: SignalAttributes -> Float -> Float -> Float -> Signal
 genEnvelope sa@(SignalAttributes sr) len indur outdur = 0 : takeWhile (>0) (map (generateEnvelope sr len indur outdur) [1..])
@@ -83,43 +97,15 @@ sineInstr sa length attrs = applyEnvelope sa length 0.01 0.01 $ genCompositeSine
 pitchToFrequency :: Float -> Float
 pitchToFrequency pitch = 440 * (2 ** ((pitch - 69) / 12))
 
-allHarmonicsWave :: SignalAttributes -> Float -> Float -> Signal
-allHarmonicsWave sa length freq = sineInstr sa length $ map (\h -> (h * freq, 1/h)) [1..20]
+approxSawtoothWave :: SignalAttributes -> Float -> Float -> Signal
+approxSawtoothWave sa length freq = sineInstr sa length $ map (\h -> (h * freq, 1/h)) [1..20]
 
 approxSquareWave :: SignalAttributes -> Float -> Float -> Signal
 approxSquareWave sa length freq = sineInstr sa length $ map (\h -> (h * freq, 1/h)) [1, 3..19]
 
+approxTriangleWave :: SignalAttributes -> Float -> Float -> Signal
+approxTriangleWave sa length freq = sineInstr sa length $ map (\(f, g) -> (f * freq, g)) harmonics
+    where harmonics = [(1, 1), (3, -1/9), (5, 1/25), (7, -1/49), (9, 1/81), (11, -1/121)]
+
 sineWave :: SignalAttributes -> Float -> Float -> Signal
-sineWave sa length freq = sineInstr sa length [(freq, 10)]
-
-approxSquareInstr :: SignalAttributes -> [String] -> Signal
-approxSquareInstr sa events = []
-
---instrumentTable :: [(String, Instrument)]
---instrumentTable = [("approxSquare", approxSquareInstr)]
-
--- Sequences are series of events that are passed to a specific instrument
--- Events are one per line, no multiline events or multiples on one line
--- Compositions are a bunch of sequences with time offsets of each other
--- Each sequence of a composition is generated independently so that randomness gets redone
-
-renderComposition :: SignalAttributes -> Composition -> Signal
-renderComposition sa composition = []
-
-data Sequence = Sequence {
-  instrument :: String,
-  events :: [String],
-  start :: Float
-} deriving (Show)
-
-readComposition :: String -> IO [Sequence]
-readComposition filename = readFile filename >>= return . lines >>= readSequences
-
-readSequences :: [String] -> IO [Sequence]
-readSequences sequences = sequence $ map readSequence sequences
-
-readSequence :: String -> IO Sequence
-readSequence sequenceSpec = let specelems = words sequenceSpec
-                                sequenceFile = specelems !! 0
-                                start = specelems !! 1 in
-                              readFile sequenceFile >>= return . lines >>= \ l -> return (Sequence (head l) (tail l) (read start))
+sineWave sa length freq = sineInstr sa length [(freq, 1)]
