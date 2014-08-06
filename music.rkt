2443616c184f432365d72c1f0e77f3222caead61
Zavire Shiran <zavire.shiran@gmail.com>
2014-08-05 21:58:29 -0500

    Refactor everything to get rid of generators, which are very slow.

--- music.rkt
+++ music.rkt
@@ -16,70 +16,80 @@
         (else (* 4 (- phase 1)))))
 
 (define (oscillator sample-rate frequency-signal func)
-  (generator ()
-             (let loop ((phase 0.0))
-               (when (> phase 1.0) (loop (- phase 1.0)))
-               (yield (func phase))
-               (loop (+ phase (/ (frequency-signal) sample-rate))))))
+  (let ((phase 0.0))
+    (lambda ()
+      (when (> phase 1.0) (set! phase (- phase 1.0)))
+      (begin0
+          (func phase)
+        (set! phase (+ phase (/ (frequency-signal) sample-rate)))))))
 
 (define (constant-signal value)
-  (generator () (let loop ()
-                  (yield value)
-                  (loop))))
+  (lambda () value))
 
 (define (add-signals . signals)
-  (generator () (let loop ((accum 0))
-                  (for ((signal signals))
-                    (set! accum (+ accum (signal))))
-                  (yield accum)
-                  (loop 0))))
+  (lambda ()
+    (for/sum ((signal signals)) (signal))))
 
 (define (gain-signal gain signal)
-  (generator () (for ((s (in-producer signal (void))))
-                  (yield s))))
+  (lambda ()
+    (* gain (signal))))
 
-; TODO: make a buffer to encode into
+;TODO: make large buffer for output
 (define (s32-file filename signal)
   (with-output-to-file filename
     (thunk
      (for ((s (in-producer signal (void))))
        (display (integer->integer-bytes s 4 #t))))
-    #:mode 'binary
-    #:exists 'truncate))
+     #:mode 'binary
+     #:exists 'truncate))
 
 (define (signal-sample-take-num num signal)
-  (generator ()
-             (for ((i (in-range num)))
-               (yield (signal)))))
+  (let ((n num))
+    (lambda ()
+      (if (> n 0)
+          (begin
+            (set! n (- n 1))
+            (signal))
+          (void)))))
 
 (define (float-signal-to-integer-signal gain signal)
-  (generator ()
-             (for ((s (in-producer signal (void))))
-               (yield (exact-round (* gain s))))))
-
-(define (append-signals . signals)
-  (generator ()
-             (for* ((signal signals)
-                    (s (in-producer signal (void))))
-               (yield s))))
+  (lambda ()
+    (let ((s (signal)))
+      (if (void? s)
+          (void)
+          (exact-round (* gain s))))))
 
 (define (segment-signal sample-rate . points)
-  (let ((segment
-         (lambda (length start end)
-           (generator ()
-                      (for ((i (in-range length)))
-                        (yield (+ start (* (/ i length) (- end start)))))))))
-    (apply append-signals
-           (for/list ((start points)
-                      (end (cdr points)))
-             (match-let (((list start-time start-value) start)
-                         ((list end-time end-value) end))
-               (segment (* sample-rate (- end-time start-time)) start-value end-value))))))
+  (let ((i 0))
+    (define (iter)
+      (if (eq? (cdr points) null)
+          (void)
+          (let ((time (/ i sample-rate))
+                (startt (first (first points)))
+                (startv (second (first points)))
+                (endt (first (second points)))
+                (endv (second (second points))))
+            (if (>= time endt)
+                (begin (set! points (cdr points))
+                       (iter))
+                (begin0
+                    (+ startv
+                       (* (- endv startv)
+                          (/ (- time startt)
+                             (- endt startt))))
+                  (set! i (+ i 1)))))))
+    iter))
+            
+(define (pitch->frequency pitch)
+  (* 440 (expt 2 (/ (- pitch 69.0) 12.0))))
 
 (module+ main #f
          (s32-file "envelope.s32" (float-signal-to-integer-signal (expt 2 28) (segment-signal 44100 '(0 0) '(0.1 1) '(0.9 1) '(1.0 0)))))
-;         (let* ((osc (gain-signal 10 (oscillator 44100 (constant-signal 10) sine-func)))
-;                (osc2 (gain-signal 100 (oscillator 44100 (add-signals osc (constant-signal 55)) sine-func)))
-;                (osc3 (oscillator 44100 (add-signals osc2 (constant-signal 220)) sine-func))
-;                (s1 (float-signal-to-integer-signal (expt 2 28) osc3)))
-;           (s32-file "music.s32" (signal-sample-take-num 44100 s1))))
+;         (require profile)
+;         (profile-thunk
+;          (thunk
+;           (let* ((osc (gain-signal 10 (oscillator 44100 (constant-signal 10) sine-func)))
+;                  (osc2 (gain-signal 100 (oscillator 44100 (add-signals osc (constant-signal 55)) sine-func)))
+;                  (osc3 (oscillator 44100 (add-signals osc2 (constant-signal 220)) sine-func))
+;                  (s1 (float-signal-to-integer-signal (expt 2 28) osc3)))
+;             (s32-file "music.s32" (signal-sample-take-num 44100 s1))))))
